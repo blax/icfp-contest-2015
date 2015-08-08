d4acc9890ac203719ccd61de3dabef8a4e3e467f
Arek Flinik <aflinik@gmail.com>
2015-08-08 02:55:46 +0200

    hai nilgh'ri UTF-oth ⬢ ⬡ ⬢

--- src/InputPresenter.hs
+++ src/InputPresenter.hs
@@ -1,6 +1,7 @@
 module InputPresenter (showProblem) where
 
 import InputTypes
+import Control.Monad (when)
 
 showProblem :: Input -> IO ()
 showProblem input = do
@@ -8,22 +9,24 @@ showProblem input = do
   showCells (iFilled input)
   putStrLn "Units"
   flip mapM_ (iUnits input) $ \unit -> do
-    showCells' ((uPivot unit, 'O') : map (\x -> (x, 'X')) (uMembers unit))
+    showCells' ((uPivot unit, '⬡') : map (\x -> (x, '⬢')) (uMembers unit))
     putStrLn ""
   putStrLn "Source length"
   print (iSourceLength input)
 
 showCells :: [Cell] -> IO ()
-showCells = showCells' . map (\x -> (x, 'X'))
+showCells = showCells' . map (\x -> (x, '⬢'))
 
 showCells' :: [(Cell, Char)] -> IO ()
 showCells' [] = return ()
 showCells' cells' = do
   flip mapM_ [minY..maxY] $ \y -> do
+    when (odd y) $ putStr " "
     flip mapM_ [minX..maxX] $ \x -> do
       case lookup (Cell x y) cells' of
         Just c -> putStr [c]
         Nothing -> putStr " "
+      putStr " "
     putStrLn ""
 
   where minX = minimum (map cX cells)
